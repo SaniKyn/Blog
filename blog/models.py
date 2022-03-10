635c2530f9c6efdc8b66c3386140455c22c7f53f
SaniKyn <gajdukevich.sani@mail.ru>
2022-03-10 23:04:46 +0300

    Добавления картинок для постов.

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
+from PIL import Image
 from django.db import models
 from django.urls import reverse, reverse_lazy
-
+_MAX_SIZE = 300
 
 class Post(models.Model):
     title = models.CharField(max_length=200, null=True)
+    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
@@ -11,6 +13,24 @@ class Post(models.Model):
     )
     date = models.DateTimeField(auto_now_add=True, null=True)
     body = models.TextField()
+
+    def save(self, *args, **kwargs):
+        super(Post, self).save(*args, **kwargs)
+
+        if self.header_image:
+            filepath = self.header_image.path
+            width = self.header_image.width
+            height = self.header_image.height
+            max_size = max(width, height)
+            if max_size > _MAX_SIZE:
+                image = Image.open(filepath)
+
+                image = image.resize(
+                    (round(width / max_size * _MAX_SIZE),
+                     round(height / max_size * _MAX_SIZE)),
+                    Image.ANTIALIAS
+                )
+                image.save(filepath)
 
     def __str__(self):
         return '\n'.join((self.title, f'{self.author.username} | {self.date}', '\n', self.body[:140]))
@@ -29,4 +49,4 @@ class Comment(models.Model):
         return f"{self.post} ⸱ {self.body}"
 
     def get_absolute_url(self):
-        return reverse_lazy("post_detail", args=[str(self.id)])
+        return reverse_lazy("post_detail", args=[str(self.post.id)])
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post, Comment
 
 
-
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -19,7 +18,7 @@ class BlogDetailView(DetailView):
 class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = ['title', 'author', 'body']
+    fields = ['title', 'author', 'body', 'header_image']
 
 
 class BlogUpdateView(UpdateView):
@@ -33,9 +32,13 @@ class BlogDeleteView(DeleteView):
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
 
+
 class AddCommentView(CreateView):
     model = Comment
     template_name = 'add_comment.html'
     fields = '__all__'
 
 
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home'
